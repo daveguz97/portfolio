e0b05e0209401cfd3c3790c3ba1be141804b6bce
David Guzman <davidguzmaninc@gmail.com>
2021-02-01 21:21:43 -0500

    Added a footer and a header component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import footerStyles from '../styles/Footer.module.scss'
+
+const Footer = () => {
+    return (
+            <footer className={footerStyles.footer}>
+                <p>David Guzman &copy; devchallenges.io</p>
+            </footer>
+    );
+}
+
+export default Footer
